2e1032b3cdf13316522ce48e25d6565de85a7d8c
Deepak <deepakdeepu3025@gmail.com>
2024-04-22 17:45:37 +0530

    added all files

--- my-app/src/Body.jsx
+++ my-app/src/Body.jsx
@@ -1,32 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Body.css';
 
-function Body(props) {
-  const { data2 } = props;
+function Body() {
+  const [inputValue, setInputValue] = useState('');
+  const [items, setItems] = useState([]);
 
-  const renderMovies = () => {
-    return data2.map((movie, index) => {
-      const movieData = Object.values(movie)[0];
-      return (
-        <div key={index} className="movie">
-          <div className="image-container">
-            <img src={movieData.imageurls} alt={movieData.moviename} />
-            <div className="movie-details">
-              <h2>{movieData.moviename}</h2>
-              <p><h3>Release Year:</h3> {movieData.releaseyears}</p>
-              <p><h3>Duration:</h3>{movieData.duration}</p>
-              <p><h3>Genres:</h3> {movieData.genres}</p>
-              <p><h3>Description:</h3> {movieData["movie description"]}</p>
-            </div>
-          </div>
-        </div>
-      );
-    });
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleAddItem = () => {
+    if (inputValue.trim() !== '') {
+      setItems([...items, { text: inputValue, checked: false }]);
+      setInputValue('');
+      setTimeout(() => {
+        alert('Item added!');
+      }, 100); // Ensuring the alert appears after the state update
+    }
+  };
+
+  const handleToggleCheck = (index) => {
+    const updatedItems = [...items];
+    updatedItems[index].checked = !updatedItems[index].checked;
+    setItems(updatedItems);
+  };
+
+  const handleDeleteItem = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+    setTimeout(() => {
+      alert('Item deleted!');
+    }, 100);
   };
 
   return (
-    <div className='main'>
-      {renderMovies()}
+    <div>
+      <div className="boxcontainer">
+      <h2>Grocery Bud</h2>
+      <div className="input">
+        <input 
+          type="text" 
+          value={inputValue} 
+          onChange={handleInputChange} 
+        />
+        <button onClick={handleAddItem}>Add item</button>
+      </div>
+      <div className="itemList">
+        {items.map((item, index) => (
+          <div className='itemList2' key={index}>
+            <input 
+              type="checkbox" 
+              checked={item.checked} 
+              onChange={() => handleToggleCheck(index)} 
+            />
+            <span style={{ textDecoration: item.checked ? 'line-through' : 'none' }}>
+              {item.text}
+            </span>
+            <button onClick={() => handleDeleteItem(index)}>Delete</button>
+          </div>
+          
+        ))}
+        </div>
+      </div>
     </div>
   );
 }
