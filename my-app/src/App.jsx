a6a8381e80643864b86d1fcf0293fe5898ef3f5a
Deepak <deepakdeepu3025@gmail.com>
2024-04-25 14:44:42 +0530

    added all files

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,12 +1,18 @@
-
 import './App.css';
-import Body from './Body';
+import LandingScreen from './screens/LandingScreen';
+import { createContext, useState } from 'react';
+import BudgetContext from './components/BudgetContext';
+
 
 function App() {
+  const totalBudget = 2000;
+  const [totalSpend, setTotalSpend] = useState(0);
+  const [expenses, setExpenses] = useState([]);
+
   return (
-    <div>
-      <Body></Body>
-      </div>
+    <BudgetContext.Provider value={{totalBudget,totalSpend,setTotalSpend, expenses,setExpenses}}>
+      <LandingScreen/>
+    </BudgetContext.Provider>
   );
 }
 
